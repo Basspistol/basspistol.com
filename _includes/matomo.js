8b6357fc7e0a334004e1a2c8f86c8320d2143a7a
Set Hallstrom <set@hallstrom.ch>
2020-11-30 17:21:10 +0100

    skip tag manager

--- _includes/matomo.js
+++ _includes/matomo.js
@@ -1,6 +1,11 @@
+{% if page.url == '/404/' %}
+
+//* Matomo *//
+
 var _paq = window._paq = window._paq || [];
-_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-_paq.push(["setCookieDomain", "*.basspistol.org"]);
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['setDocumentTitle',  '404/URL = ' +  encodeURIComponent(document.location.pathname+document.location.search) + '/From = ' + encodeURIComponent(document.referrer)]);
+_paq.push(["setCookieDomain", "*.basspistol.com"]);
 _paq.push(["setDomains", ["*.basspistol.org","*.basspistol.com","*.pauvrecoude.basspistol.com","*.paxnion.basspistol.com","*.radio.basspistol.com","*.sakrecoer.basspistol.com","*.sakrecoer.com","*.sethybowoy.basspistol.com","*.setto.basspistol.com","*.tfb.basspistol.com","*.v.basspistol.org"]]);
 _paq.push(["enableCrossDomainLinking"]);
 _paq.push(["disableCookies"]);
@@ -13,3 +18,28 @@ _paq.push(['enableLinkTracking']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.type='text/javascript'; g.async=true; g.src='https://cdn.matomo.cloud/terminalnetwork.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
 })();
+//* End Matomo *//
+
+{% else %}
+
+//* Matomo *//
+
+var _paq = window._paq = window._paq || [];
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(["setCookieDomain", "*.basspistol.com"]);
+_paq.push(["setDomains", ["*.basspistol.org","*.basspistol.com","*.pauvrecoude.basspistol.com","*.paxnion.basspistol.com","*.radio.basspistol.com","*.sakrecoer.basspistol.com","*.sakrecoer.com","*.sethybowoy.basspistol.com","*.setto.basspistol.com","*.tfb.basspistol.com","*.v.basspistol.org"]]);
+_paq.push(["enableCrossDomainLinking"]);
+_paq.push(["disableCookies"]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://terminalnetwork.matomo.cloud/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '{{ site.matomo_site_id }}']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.src='https://cdn.matomo.cloud/terminalnetwork.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+})();
+
+//* End Matomo *//
+
+{% endif %}
